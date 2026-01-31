0d5cb0fa1d77dd66bbde9910989a516174bb49d6
Dreamlike <sbersenev@yandex.ru>
2026-01-31 18:37:49 +0300

    Параметризовал тест вопросов и ответов

--- src/test/java/YandexScooterSite/HomePage.java
+++ src/test/java/YandexScooterSite/HomePage.java
@@ -9,33 +9,34 @@ import static org.openqa.selenium.support.locators.RelativeLocator.with;
 @Getter
 public class HomePage {
     private final WebDriver driver;
+    private String questionText;
     // Кнопка куки "Да все привыкли"
-    private final By cookie =
+    private final By cookieAcceptButton =
             By.xpath("//*[text()='да все привыкли']");
     // Вопрос 1. Сколько это стоит? И как оплатить?
-    private final By question_1 =
-            By.xpath("//*[text()='Сколько это стоит? И как оплатить?']");
-    // Вопрос 2. Хочу сразу несколько самокатов! Так можно?
-    private final By question_2 =
-            By.xpath("//*[text()='Хочу сразу несколько самокатов! Так можно?']");
-    // Вопрос 3. Как рассчитывается время аренды?
-    private final By question_3 =
-            By.xpath("//*[text()='Как рассчитывается время аренды?']");
-    // Вопрос 4. Можно ли заказать самокат прямо на сегодня?
-    private final By question_4 =
-            By.xpath("//*[text()='Можно ли заказать самокат прямо на сегодня?']");
-    // Вопрос 5. Можно ли продлить заказ или вернуть самокат раньше?
-    private final By question_5 =
-            By.xpath("//*[text()='Можно ли продлить заказ или вернуть самокат раньше?']");
-    // Вопрос 6. Вы привозите зарядку вместе с самокатом?
-    private final By question_6 =
-            By.xpath("//*[text()='Вы привозите зарядку вместе с самокатом?']");
-    // Вопрос 7. Можно ли отменить заказ?
-    private final By question_7 =
-            By.xpath("//*[text()='Можно ли отменить заказ?']");
-    // Вопрос 8. Я жизу за МКАДом, привезёте?
-    private final By question_8 =
-            By.xpath("//*[text()='Я жизу за МКАДом, привезёте?']");
+//    private final By question_1 =
+//            By.xpath("//*[text()='Сколько это стоит? И как оплатить?']");
+//    // Вопрос 2. Хочу сразу несколько самокатов! Так можно?
+//    private final By question_2 =
+//            By.xpath("//*[text()='Хочу сразу несколько самокатов! Так можно?']");
+//    // Вопрос 3. Как рассчитывается время аренды?
+//    private final By question_3 =
+//            By.xpath("//*[text()='Как рассчитывается время аренды?']");
+//    // Вопрос 4. Можно ли заказать самокат прямо на сегодня?
+//    private final By question_4 =
+//            By.xpath("//*[text()='Можно ли заказать самокат прямо на сегодня?']");
+//    // Вопрос 5. Можно ли продлить заказ или вернуть самокат раньше?
+//    private final By question_5 =
+//            By.xpath("//*[text()='Можно ли продлить заказ или вернуть самокат раньше?']");
+//    // Вопрос 6. Вы привозите зарядку вместе с самокатом?
+//    private final By question_6 =
+//            By.xpath("//*[text()='Вы привозите зарядку вместе с самокатом?']");
+//    // Вопрос 7. Можно ли отменить заказ?
+//    private final By question_7 =
+//            By.xpath("//*[text()='Можно ли отменить заказ?']");
+//    // Вопрос 8. Я жизу за МКАДом, привезёте?
+//    private final By question_8 =
+//            By.xpath("//*[text()='Я жизу за МКАДом, привезёте?']");
 
     // Кнопка "Заказать" верхняя
     // Кнопка "Заказать" нижняя
@@ -59,15 +60,30 @@ public class HomePage {
         this.driver = driver;
     }
 
+    public HomePage(WebDriver driver, String questionText) {
+        this.driver = driver;
+        this.questionText = questionText;
+    }
+
+    private By getQuestionPath() {
+        return By.xpath("//*[text()='" + questionText + "']");
+    }
+
     public void clickElement(By pathToElement) {
         Actions actions = new Actions(driver);
         actions.scrollToElement(driver.findElement(pathToElement)).perform();
         driver.findElement(pathToElement).click();
     }
 
-    public String getElementText(By pathToElement) {
+    public void clickQuestion() {
         Actions actions = new Actions(driver);
-        By answerText = with(By.xpath("//*[contains(@id, 'accordion__panel')]")).near(pathToElement);
+        actions.scrollToElement(driver.findElement(getQuestionPath())).perform();
+        driver.findElement(getQuestionPath()).click();
+    }
+
+    public String getAnswerText() {
+        Actions actions = new Actions(driver);
+        By answerText = with(By.xpath("//*[contains(@id, 'accordion__panel')]")).near(getQuestionPath());
         actions.scrollToElement(driver.findElement(answerText)).perform();
         return driver.findElement(answerText).getText();
     }
--- src/test/java/YandexScooterTests.java
+++ src/test/java/YandexScooterTests.java
@@ -1,14 +1,19 @@
 import YandexScooterSite.HomePage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class YandexScooterTests {
     private WebDriver driver;
+
 
     @BeforeEach
     public void setUp() {
@@ -16,15 +21,41 @@ public class YandexScooterTests {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
 
-    @Test
-    void questionAnswerTest() {
+    @ParameterizedTest
+    @MethodSource("questionsAnswersProvider")
+    void questionAnswerTest(String questionText, String expected) {
         driver.get("https://qa-scooter.praktikum-services.ru/");
-        HomePage objHomePage = new HomePage(driver);
-        objHomePage.clickElement(objHomePage.getCookie());
-        objHomePage.clickElement(objHomePage.getQuestion_1());
-        String actual = objHomePage.getElementText(objHomePage.getQuestion_1());
-        String expected = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
-        assertEquals(expected, actual, "Текст вопроса и ответа не совпадает");
+        HomePage objHomePage = new HomePage(driver, questionText);
+        objHomePage.clickElement(objHomePage.getCookieAcceptButton());
+        objHomePage.clickQuestion();
+        assertEquals(expected, objHomePage.getAnswerText(), "Текст вопроса и ответа не совпадает");
+    }
+
+    private static Stream<Arguments> questionsAnswersProvider() {
+        return Stream.of(
+                Arguments.of("Сколько это стоит? И как оплатить?",
+                        "Сутки — 400 рублей. Оплата курьеру — наличными или картой."),
+                Arguments.of("Хочу сразу несколько самокатов! Так можно?",
+                        "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, " +
+                                "можете просто сделать несколько заказов — один за другим."),
+                Arguments.of("Как рассчитывается время аренды?",
+                        "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня." +
+                                " Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру." +
+                                " Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."),
+                Arguments.of("Можно ли заказать самокат прямо на сегодня?",
+                        "Только начиная с завтрашнего дня. Но скоро станем расторопнее."),
+                Arguments.of("Можно ли продлить заказ или вернуть самокат раньше?",
+                        "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому" +
+                                " номеру 1010."),
+                Arguments.of("Вы привозите зарядку вместе с самокатом?",
+                        "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете" +
+                                " кататься без передышек и во сне. Зарядка не понадобится."),
+                Arguments.of("Можно ли отменить заказ?",
+                        "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим." +
+                                " Все же свои."),
+                Arguments.of("Я жизу за МКАДом, привезёте?",
+                        "Да, обязательно. Всем самокатов! И Москве, и Московской области.")
+        );
     }
 
     @AfterEach
