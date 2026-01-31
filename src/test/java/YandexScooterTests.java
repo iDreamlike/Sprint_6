6a69052c6a5674ac92e51788cee43a63c3df7a9f
Dreamlike <sbersenev@yandex.ru>
2026-01-31 20:21:20 +0300

    Вынес тестовые данные в отдельный класс и добавил тест проверки заказа

--- src/test/java/YandexScooterTests.java
+++ src/test/java/YandexScooterTests.java
@@ -1,61 +1,48 @@
-import YandexScooterSite.HomePage;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import yandex_scooter_site.HomePage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
-import java.util.stream.Stream;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class YandexScooterTests {
     private WebDriver driver;
 
-
     @BeforeEach
     public void setUp() {
         driver = new ChromeDriver();
+//        driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
 
     @ParameterizedTest
-    @MethodSource("questionsAnswersProvider")
+    @MethodSource("test_data.YandexScooterTestData#questionsAnswersProvider")
     void questionAnswerTest(String questionText, String expected) {
         driver.get("https://qa-scooter.praktikum-services.ru/");
         HomePage objHomePage = new HomePage(driver, questionText);
-        objHomePage.clickElement(objHomePage.getCookieAcceptButton());
+        objHomePage.clickAcceptCookieButton();
         objHomePage.clickQuestion();
         assertEquals(expected, objHomePage.getAnswerText(), "Текст вопроса и ответа не совпадает");
     }
 
-    private static Stream<Arguments> questionsAnswersProvider() {
-        return Stream.of(
-                Arguments.of("Сколько это стоит? И как оплатить?",
-                        "Сутки — 400 рублей. Оплата курьеру — наличными или картой."),
-                Arguments.of("Хочу сразу несколько самокатов! Так можно?",
-                        "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, " +
-                                "можете просто сделать несколько заказов — один за другим."),
-                Arguments.of("Как рассчитывается время аренды?",
-                        "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня." +
-                                " Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру." +
-                                " Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."),
-                Arguments.of("Можно ли заказать самокат прямо на сегодня?",
-                        "Только начиная с завтрашнего дня. Но скоро станем расторопнее."),
-                Arguments.of("Можно ли продлить заказ или вернуть самокат раньше?",
-                        "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому" +
-                                " номеру 1010."),
-                Arguments.of("Вы привозите зарядку вместе с самокатом?",
-                        "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете" +
-                                " кататься без передышек и во сне. Зарядка не понадобится."),
-                Arguments.of("Можно ли отменить заказ?",
-                        "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим." +
-                                " Все же свои."),
-                Arguments.of("Я жизу за МКАДом, привезёте?",
-                        "Да, обязательно. Всем самокатов! И Москве, и Московской области.")
-        );
+    @Test
+    void scooterOrderPositiveTest() {
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+        HomePage objHomePage = new HomePage(driver);
+        objHomePage.clickAcceptCookieButton();
+        objHomePage.clickHeaderOrderButton();
+        objHomePage.createOrder(
+                "Вася",
+                "Петров",
+                "Забугоркино",
+                "11111111111");
+        assertTrue(objHomePage.getCompleteOrderTitleText().contains("Заказ оформлен"));
     }
 
     @AfterEach
